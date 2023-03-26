b1e49d62e5a48c3e811c63bbfd11b2c788f2a5ce
ImJustNon <Im_Just_Non@hotmail.com>
2023-03-26 19:24:29 +0700

    Add checkjs

--- routes/check.js
+++ routes/check.js
@@ -7,21 +7,24 @@ const urlEncoded = bodyParser.urlencoded({
 });
 const db = require("../database/connect.js");
 
-router.get("/api/check", urlEncoded, async(req, res) => {
+router.post("/api/check", urlEncoded, async(req, res) => {
     
     const rows = db.prepare('SELECT * FROM turn_on_api').all();
     // status 1 = ON
     // status 0 = OFF
     if(rows[0].status === 1){
-        return res.json({
+        return res.sendStatus(200);
+        /*return res.status(200).json({
             status: 1,
-        });
+        });*/
     }
     if(rows[0].status === 0){
-        return res.json({
+        return res.sendStatus(404);
+        /*return res.status(404).json({
             status: 0,
-        });
+        });*/
     }
+    console.log("[Alert] Checked");
 });
 
 module.exports = router;
